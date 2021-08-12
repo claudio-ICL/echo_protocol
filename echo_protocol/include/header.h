93310c2a0dd9d3cce00264c522eb2b1f9d59edd4
Claudio Bellani <c.bellani17@imperial.ac.uk>
2021-08-12 15:35:10 +0200

    Echo protocol - header file
     On branch master
     Changes to be committed:
    	new file:   echo_protocol/include/header.h

---
+++ echo_protocol/include/header.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <ctype.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct sockaddr_input {
+  short sin_family;
+  int port_num;
+  unsigned long s_addr;
+};
+
+struct sockaddr_input input_from_commandline(int argc, char *argv[]);
+
+struct sockaddr_in instantiate_sockaddr_in(struct sockaddr_input);
+
+void capitalize_str(char *, char *);
